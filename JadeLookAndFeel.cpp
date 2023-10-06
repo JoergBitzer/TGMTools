e2eb58dc70a0bb35019427724fa09e1602e49227
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2023-10-07 00:19:35 +0200

    added juce as namespace

--- JadeLookAndFeel.cpp
+++ JadeLookAndFeel.cpp
@@ -2,28 +2,28 @@
 
 JadeLookAndFeel::JadeLookAndFeel()
 {
-	setColour(ResizableWindow::backgroundColourId, JadeWhite);
+	setColour(juce::ResizableWindow::backgroundColourId, JadeWhite);
 
-	setColour(Slider::ColourIds::backgroundColourId, JadeGray);
-	setColour(Slider::ColourIds::thumbColourId, JadeRed);
-	setColour(Slider::ColourIds::trackColourId, JadeGray);
-	setColour(Slider::ColourIds::textBoxTextColourId, JadeGray);
+	setColour(juce::Slider::ColourIds::backgroundColourId, JadeGray);
+	setColour(juce::Slider::ColourIds::thumbColourId, JadeRed);
+	setColour(juce::Slider::ColourIds::trackColourId, JadeGray);
+	setColour(juce::Slider::ColourIds::textBoxTextColourId, JadeGray);
 
-	setColour(Label::ColourIds::textColourId, JadeGray);
+	setColour(juce::Label::ColourIds::textColourId, JadeGray);
 
-	setColour(TextButton::ColourIds::buttonColourId, JadeGray);
-	setColour(TextButton::ColourIds::buttonOnColourId, Colours::darkgrey);
-	setColour(TextButton::ColourIds::textColourOnId, JadeLightRed1);
-	setColour(TextButton::ColourIds::textColourOffId, JadeWhite);
+	setColour(juce::TextButton::ColourIds::buttonColourId, JadeGray);
+	setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::darkgrey);
+	setColour(juce::TextButton::ColourIds::textColourOnId, JadeLightRed1);
+	setColour(juce::TextButton::ColourIds::textColourOffId, JadeWhite);
 	m_fontSize = 12;
 
 	setDefaultLookAndFeel(this);
 }
 
-void JadeLookAndFeel::drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos, const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider)
+void JadeLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos, const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider)
 {
 	juce::ignoreUnused (slider);
-	auto radius = jmin(width / 2, height / 2) - 4.0f;
+	auto radius = juce::jmin(width / 2, height / 2) - 4.0f;
 	auto centreX = x + width * 0.5f;
 	auto centreY = y + height * 0.5f;
 	auto rx = centreX - radius;
@@ -36,16 +36,16 @@ void JadeLookAndFeel::drawRotarySlider(Graphics& g, int x, int y, int width, int
 	g.fillEllipse(rx, ry, rw, rw);
 
 	// outline
-	Colour c1 = Colour::fromFloatRGBA(1.3*JadeRed.getFloatRed()*sliderPos,3.0*JadeRed.getFloatGreen()*sliderPos,3.0*JadeRed.getFloatBlue()*sliderPos,1.0);
+	juce::Colour c1 = juce::Colour::fromFloatRGBA(1.3*JadeRed.getFloatRed()*sliderPos,3.0*JadeRed.getFloatGreen()*sliderPos,3.0*JadeRed.getFloatBlue()*sliderPos,1.0);
 	g.setColour(c1);
 	//g.drawEllipse(rx, ry, rw, rw, 5.0);
-	g.drawEllipse(rx, ry, rw, rw, jmax(int(width*0.07),5));
+	g.drawEllipse(rx, ry, rw, rw, juce::jmax(int(width*0.07),5));
 
 	// Point
-	int PointSize = jmax(width/6,10);
-	Path p;
+	int PointSize = juce::jmax(width/6,10);
+	juce::Path p;
 	p.addEllipse(-PointSize / 2, -0.95 * radius, PointSize, PointSize);
-	p.applyTransform(AffineTransform::rotation(angle).translated(centreX, centreY));
+	p.applyTransform(juce::AffineTransform::rotation(angle).translated(centreX, centreY));
 
 	g.setColour(JadeRed);
 	g.fillPath(p);
--- JadeLookAndFeel.h
+++ JadeLookAndFeel.h
@@ -14,32 +14,33 @@
 
 #pragma once
 
-#include <JuceHeader.h>
+//#include <JuceHeader.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 
 /**
  * @brief define the Jade color scheme given by the CD 
  */
-const auto JadeWhite(Colour::fromFloatRGBA(0.95, 0.95, 0.95, 1.0));
-const auto JadeRed(Colour::fromFloatRGBA(0.890196078431373, 0.023529411764706, 0.074509803921569, 1.0));
-const auto JadeTeal(Colour::fromFloatRGBA(0.352941176470588, 0.372549019607843, 0.337254901960784, 1.0));
-const auto JadeGray(Colour::fromFloatRGBA(0.356862745098039, 0.372549019607843, 0.341176470588235, 1.0));
+const auto JadeWhite(juce::Colour::fromFloatRGBA(0.95, 0.95, 0.95, 1.0));
+const auto JadeRed(juce::Colour::fromFloatRGBA(0.890196078431373, 0.023529411764706, 0.074509803921569, 1.0));
+const auto JadeTeal(juce::Colour::fromFloatRGBA(0.352941176470588, 0.372549019607843, 0.337254901960784, 1.0));
+const auto JadeGray(juce::Colour::fromFloatRGBA(0.356862745098039, 0.372549019607843, 0.341176470588235, 1.0));
 
-const auto JadeLightRed1(Colour::fromFloatRGBA(0.956862745098039, 0.631372549019608, 0.552941176470588, 1.0));
-const auto JadeLightRed2(Colour::fromFloatRGBA(0.968627450980392, 0.733333333333333, 0.682352941176471, 1.0));
-const auto JadeLightRed3(Colour::fromFloatRGBA(0.980392156862745, 0.807843137254902, 0.776470588235294, 1.0));
+const auto JadeLightRed1(juce::Colour::fromFloatRGBA(0.956862745098039, 0.631372549019608, 0.552941176470588, 1.0));
+const auto JadeLightRed2(juce::Colour::fromFloatRGBA(0.968627450980392, 0.733333333333333, 0.682352941176471, 1.0));
+const auto JadeLightRed3(juce::Colour::fromFloatRGBA(0.980392156862745, 0.807843137254902, 0.776470588235294, 1.0));
 // JadeLightRed4 = [0.988235294117647 0.890196078431373 0.870588235294118];
 #define MIN_COMBO_WITH_PRESET 120
 #define MIN_ELEMENT_DIST_PRESET 10
 #define MIN_BUTTON_WIDTH_PRESET 40
 #define MIN_ELEMENT_HEIGHT_PRESET 20
 // Category Lists for different usage (max number of Cat is 20);
-const StringArray JadeSynthCategories("Unknown", "Lead", "Brass", "Template", "Bass",
+const juce::StringArray JadeSynthCategories("Unknown", "Lead", "Brass", "Template", "Bass",
 	"Key", "Organ" , "Pad", "Drums_Perc", "SpecialEffect","Sequence", "String" );
 /**
  * @brief the Look and Feel implements the rotary knob in a special way and adjust the font size according to scale
  * 
  */
-class JadeLookAndFeel : public LookAndFeel_V4
+class JadeLookAndFeel : public juce::LookAndFeel_V4
 {
 public:
 	JadeLookAndFeel();
@@ -47,22 +48,22 @@ public:
 
 private:
 	int m_fontSize;
-	void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos,
-		const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider) override;
+	void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+		const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider) override;
 
-void drawButtonText (Graphics& g, TextButton& button,
+void drawButtonText (juce::Graphics& g, juce::TextButton& button,
 		bool /*shouldDrawButtonAsHighlighted*/, bool /*shouldDrawButtonAsDown*/)
 	{
 		// Font font (button.getHeight () * 0.6f);
-		Font font (m_fontSize);
+		juce::Font font (m_fontSize);
 
 		g.setFont (font);
-		g.setColour (button.findColour (button.getToggleState () ? TextButton::textColourOnId
-			: TextButton::textColourOffId)
+		g.setColour (button.findColour (button.getToggleState () ? juce::TextButton::textColourOnId
+			: juce::TextButton::textColourOffId)
 			.withMultipliedAlpha (button.isEnabled () ? 1.0f : 0.5f));
 
 		const int yIndent = button.proportionOfHeight (0.1f);
-		const int cornerSize = jmin (button.getHeight (), button.getWidth ()) / 2;
+		const int cornerSize = juce::jmin (button.getHeight (), button.getWidth ()) / 2;
 
 		const int leftIndent = cornerSize / (button.isConnectedOnLeft () ?
                   yIndent * 2 : yIndent);
@@ -73,27 +74,27 @@ void drawButtonText (Graphics& g, TextButton& button,
 		if (textWidth > 0)
 			g.drawFittedText (button.getButtonText (),
 				leftIndent, yIndent, textWidth, button.getHeight () - yIndent * 2,
-				Justification::centred, 2, 0.5f);
+				juce::Justification::centred, 2, 0.5f);
 	}	
-	Font getComboBoxFont (ComboBox &c)
+	juce::Font getComboBoxFont (juce::ComboBox &c)
 	{
 		juce::ignoreUnused (c);
 		//return Font(c.getHeight () * 0.6f);
-		return Font(m_fontSize);
+		return juce::Font(m_fontSize);
 
 	}	
-	Font 	getPopupMenuFont ()
+	juce::Font 	getPopupMenuFont ()
 	{
-		return Font(m_fontSize);
+		return juce::Font(m_fontSize);
 	}
-	Font getLabelFont()
+	juce::Font getLabelFont()
 	{
-		return Font(m_fontSize);
+		return juce::Font(m_fontSize);
 	}
-	Font getLabelFont(Label &l)
+	juce::Font getLabelFont(juce::Label &l)
 	{
-		l.setFont(Font(m_fontSize));
-		return Font(m_fontSize);
+		l.setFont(juce::Font(m_fontSize));
+		return juce::Font(m_fontSize);
 	}
 
 
